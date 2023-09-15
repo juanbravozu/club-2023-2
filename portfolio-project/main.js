e4b261e0d1d1cbac85dab00a513fbdda818a6406
juanbravozu <juanda.bravo98@gmail.com>
2023-09-15 15:01:25 -0500

    Club 14-09-2023
    What is included:
    
    - Refactors front-end project to Vite
    - Refactors styles to sass
    - Adds navigation, banner and footer to index.html

--- portfolio-project/main.js
+++ portfolio-project/main.js
@@ -1,10 +1,8 @@
-import { projectsData } from "./data.js";
-
 const BASE_URL = 'http://localhost:1337/api/';
 
 window.addEventListener('DOMContentLoaded', async () => {
   try {
-    const section = document.querySelector('.projects-container');
+    const section = document.querySelector('.projects__list');
   
     const res = await fetch(`${BASE_URL}projects?populate=*`);
     const {data} = await res.json();
@@ -20,18 +18,18 @@ window.addEventListener('DOMContentLoaded', async () => {
         }
       }
     }) => {
-      const card = document.createElement('article');
-      card.classList.add('project-card');
+      const card = document.createElement('li');
+      card.classList.add('card');
       card.innerHTML = `
-        <a href="#" class="project-anchor" aria-label="Más información sobre Mi proyecto de gatos">
-          <div class="image-container">
+        <a href="#" class="card__anchor" aria-label="Más información sobre Mi proyecto de gatos">
+          <div class="card__image-wrapper">
             <picture>
-              <img src="http://localhost:1337${thumbnail.data.attributes.url}" alt="${thumbnail.data.attributes.alternativeText}">
+              <img class="card__image" src="http://localhost:1337${thumbnail.data.attributes.url}" alt="${thumbnail.data.attributes.alternativeText}">
             </picture>
           </div>
-          <h3 class="project-title">${title}</h3>
-          <ul class="tags-container">
-            ${categoriesList.map(({attributes}) => `<div class="tag">${attributes.category}</div>`)}
+          <h3 class="card__title">${title}</h3>
+          <ul class="card__tags-list">
+            ${categoriesList.map(({attributes}) => `<li class="card__tag">${attributes.category}</li>`)}
           </ul>
         </a>`;
       section.appendChild(card);
